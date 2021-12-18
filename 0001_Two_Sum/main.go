4d6b98b76832b00c957bc13ed4a68c3746f1c250
小娄 <75350727+Ureylou666@users.noreply.github.com>
2021-12-19 00:21:12 +0800

    Create main.go

---
+++ 0001_Two_Sum/main.go
@@ -0,0 +1,10 @@
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for k, v := range nums {
+		if idx, ok := m[target-v]; ok {
+			return []int{idx, k}
+		}
+		m[v] = k
+	}
+	return nil
+}
