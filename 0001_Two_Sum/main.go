1234c20a7f2c92e8fb0fcaecdb442dad37452c46
小娄 <75350727+Ureylou666@users.noreply.github.com>
2021-12-19 23:23:36 +0800

    Update main.go

--- 0001_Two_Sum/main.go
+++ 0001_Two_Sum/main.go
@@ -1,3 +1,18 @@
+O(n2)算法 
+暴利枚举即可
+func twoSum(nums []int, target int) []int {
+	for i:=0;i<len(nums);i++{
+		for j:=i+1;j<len(nums);j++{
+			if nums[i]+nums[j]==target {
+				return []int{i,j}
+			}
+		}
+	}
+	return nil
+}
+
+还有一种 hashmap
+
 func twoSum(nums []int, target int) []int {
 	m := make(map[int]int)
 	for k, v := range nums {
